903ca7d2d104215221fd37d46889511a8a681414
Thibaud Dauce <thibaud@dauce.fr>
2016-05-01 13:34:51 +0200

    New version with lists

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,136 @@
 module Lib where
 
-import qualified Data.Map as M
+import Control.Monad
+import Data.List
+import Data.Maybe (isJust)
 
 type Colonne = Int
 type Ligne = Int
 type Score = Int
-data Couleur = Jaune | Rouge | Bleu | Vert | Rose deriving (Show, Eq)
-type Grille = M.Map (Colonne, Ligne) Couleur
+data Couleur = Jaune | Rouge | Bleu | Vert | Rose | Crane deriving (Show, Eq)
+type Grille = [[Couleur]]
 
-premiereLigneVide :: Grille -> Colonne -> Ligne
-premiereLigneVide grille colonne = testLigne grille colonne 1
-    where testLigne grille colonne ligne = case M.lookup (colonne, ligne) grille of
-            Just couleur -> testLigne grille colonne (ligne + 1)
-            Nothing -> ligne
+hauteur :: Grille -> Colonne -> Ligne
+hauteur grille colonne = length (grille !! colonne)
 
-ajouter :: Grille -> Colonne -> Couleur -> Grille
-ajouter grille colonne couleur = M.insert (colonne, premiereLigneVide grille colonne) couleur grille
+replaceNth :: Int -> (a -> a) -> [a] -> [a]
+replaceNth n updateFunction (x:xs)
+    | n == 0 = (updateFunction x):xs
+    | otherwise = x:replaceNth (n-1) updateFunction xs
 
-ajouterDeuxBlocs :: Grille -> Colonne -> Couleur -> Grille
-ajouterDeuxBlocs grille colonne couleur = M.insert (colonne, premiereLigneVide') couleur $ M.insert (colonne, premiereLigneVide' + 1) couleur grille
-    where premiereLigneVide' = premiereLigneVide grille colonne
+ajouter :: Colonne -> Couleur -> Grille -> Grille
+ajouter colonne couleur grille = replaceNth colonne (const $ reverse (couleur:(reverse $ grille !! colonne))) grille
+
+construireCouleursSuivante :: IO [Couleur]
+construireCouleursSuivante = do
+    couleursSuivantes <- replicateM 8 getLine
+    return (map (\x -> charVersCouleur $ x!!0) couleursSuivantes)
+
+construireGrille :: IO Grille
+construireGrille = do
+    chainesDeCaracteres <- replicateM 12 getLine
+    return (ajouterPlusieursChainesDeCaracteres chainesDeCaracteres [[], [], [], [], [], []])
+
+choisirColonne :: Couleur -> Grille -> Colonne
+choisirColonne couleur grille = maximumBy compare' [0..5]
+    where
+        compare' colonne1 colonne2 = compare (getScore colonne1) (getScore colonne2)
+        getScore colonne = (snd $ simplifierGrille (ajouterDeuxBlocs colonne couleur grille)) - length (grille!!colonne)
+
+ajouterPlusieursChainesDeCaracteres :: [String] -> Grille -> Grille
+ajouterPlusieursChainesDeCaracteres [] grille = grille
+ajouterPlusieursChainesDeCaracteres (x:xs) grille = ajouterPlusieursChainesDeCaracteres xs $ ajouterChaineDeCaractere x grille
+
+ajouterChaineDeCaractere :: String -> Grille -> Grille
+ajouterChaineDeCaractere entree grille = zipWith merge entree grille
+    where
+        merge '.' colonne = colonne
+        merge '0' colonne = Crane:colonne
+        merge x colonne = (charVersCouleur x):colonne
+
+charVersCouleur :: Char -> Couleur
+charVersCouleur '1' = Bleu
+charVersCouleur '2' = Jaune
+charVersCouleur '3' = Rouge
+charVersCouleur '4' = Vert
+charVersCouleur '5' = Rose
+
+ajouterDeuxBlocs :: Colonne -> Couleur -> Grille -> Grille
+ajouterDeuxBlocs colonne couleur grille = ajouter colonne couleur $ ajouter colonne couleur grille
+
+simplifierGrille :: Grille -> (Grille, Int)
+simplifierGrille grille = simplifierGrilleMarquee grilleMarquee 0
+    where grilleMarquee = map (map (\x -> (False, x))) grille
+
+simplifierGrilleMarquee :: [[(Bool, Couleur)]] -> Int -> (Grille, Int)
+simplifierGrilleMarquee grilleMarquee score = case premierElementNonMarque of
+        (Just colonne, Just ligne) -> simplifierGrilleMarquee' $ parcourirGrilleAPartirDe colonne ligne grilleMarquee
+        otherwise -> (map (map snd) grilleMarquee, score)
+    where
+        premierElementNonMarque = (findIndex isJust colonneAvecElementNonMarques, msum colonneAvecElementNonMarques)
+        colonneAvecElementNonMarques = map (findIndex (not . fst)) grilleMarquee
+        simplifierGrilleMarquee' (nouvelleGrilleMarquee, nouveauScore) = simplifierGrilleMarquee nouvelleGrilleMarquee (nouveauScore + score)
+
+modifierElement :: (a -> a) -> Colonne -> Ligne -> [[a]] -> [[a]]
+modifierElement fn colonne ligne grille = replaceNth colonne (const nouvelleColonne) grille
+    where nouvelleColonne = replaceNth ligne fn (grille !! colonne)
+
+parcourirGrilleAPartirDe :: Colonne -> Ligne -> [[(Bool, Couleur)]] -> ([[(Bool, Couleur)]], Int)
+parcourirGrilleAPartirDe colonne ligne grilleMarquee = if nombreDeCoupFinal < 4 then (virerMaybe grilleMarquee grilleMarqueeFinale, 0) else (virerNothings grilleMarqueeFinale, nombreDeCoupFinal)
+    where
+        (grilleMarqueeFinale, nombreDeCoupFinal) = parcourir couleurCourante colonne ligne grilleMarqueeMaybe 1
+        grilleMarqueeMaybe = supprimerBloc colonne ligne $ map (map (\(marque, couleur) -> (marque, Just couleur))) grilleMarquee
+        couleurCourante = snd $ (grilleMarquee !! colonne) !! ligne
+
+blocEstNonMarqueEtMemeCouleur :: Couleur -> Colonne -> Ligne -> [[(Bool, Maybe Couleur)]] -> [[(Bool, Maybe Couleur)]]
+blocEstNonMarqueEtMemeCouleur couleur colonne ligne grille
+    | colonne < 0 = grille
+    | ligne < 0 = grille
+    | colonne >= length grille = grille
+    | ligne >= length blocs = grille
+    | otherwise = case bloc of
+        (False, Just x) -> if (x == couleur || x == Crane) then nouvelleGrilleSucces else grille
+        (True, Just x) -> if x == Crane then nouvelleGrilleSucces else grille
+        otherwise -> grille
+    where
+        blocs = grille !! colonne
+        bloc = blocs !! ligne
+        nouvelleGrilleSucces = supprimerBloc colonne ligne grille
+
+supprimerBloc :: Colonne -> Ligne -> [[(Bool, Maybe Couleur)]] -> [[(Bool, Maybe Couleur)]]
+supprimerBloc = modifierElement (\_ -> (True, Nothing))
+
+parcourir :: Couleur -> Colonne -> Ligne -> [[(Bool, Maybe Couleur)]] -> Int -> ([[(Bool, Maybe Couleur)]], Int)
+parcourir Crane _ _ grilleMarqueeMaybe nombreDeCoup = (grilleMarqueeMaybe, nombreDeCoup)
+parcourir couleurCourante colonne ligne grilleMarqueeMaybe nombreDeCoup
+    | grilleMarqueeMaybe /= nouvelleGrilleGauche = case parcourirAGauche nouvelleGrilleGauche of
+        (nouvelleGrille, nombreDeCoupAjoute) -> parcourir couleurCourante colonne ligne nouvelleGrille (nombreDeCoup + nombreDeCoupAjoute)
+    | grilleMarqueeMaybe /= nouvelleGrilleDroite = case parcourirADroite nouvelleGrilleDroite of
+        (nouvelleGrille, nombreDeCoupAjoute) -> parcourir couleurCourante colonne ligne nouvelleGrille (nombreDeCoup + nombreDeCoupAjoute)
+    | grilleMarqueeMaybe /= nouvelleGrilleHaut = case parcourirEnHaut nouvelleGrilleHaut of
+        (nouvelleGrille, nombreDeCoupAjoute) -> parcourir couleurCourante colonne ligne nouvelleGrille (nombreDeCoup + nombreDeCoupAjoute)
+    | grilleMarqueeMaybe /= nouvelleGrilleBas = case parcourirEnBas nouvelleGrilleBas of
+        (nouvelleGrille, nombreDeCoupAjoute) -> parcourir couleurCourante colonne ligne nouvelleGrille (nombreDeCoup + nombreDeCoupAjoute)
+    | otherwise = (grilleMarqueeMaybe, nombreDeCoup)
+    where
+        blocEstNonMarqueEtMemeCouleur' x y = blocEstNonMarqueEtMemeCouleur couleurCourante x y grilleMarqueeMaybe
+        parcourir' x y g = parcourir couleurCourante x y g 1
+        nouvelleGrilleGauche = blocEstNonMarqueEtMemeCouleur' (colonne - 1) ligne
+        parcourirAGauche = parcourir' (colonne - 1) ligne
+        nouvelleGrilleDroite = blocEstNonMarqueEtMemeCouleur' (colonne + 1) ligne
+        parcourirADroite = parcourir' (colonne + 1) ligne
+        nouvelleGrilleHaut = blocEstNonMarqueEtMemeCouleur' colonne (ligne + 1)
+        parcourirEnHaut = parcourir' colonne (ligne + 1)
+        nouvelleGrilleBas = blocEstNonMarqueEtMemeCouleur' colonne (ligne - 1)
+        parcourirEnBas = parcourir' colonne (ligne + 1)
+
+virerMaybe :: [[(Bool, Couleur)]] -> [[(Bool, Maybe Couleur)]] -> [[(Bool, Couleur)]]
+virerMaybe grilleCouleurs grilleMarques = map (map (\((_, couleur), (marque, _)) -> (marque, couleur))) megaZip
+    where megaZip = zipWith (zip) grilleCouleurs grilleMarques
+
+virerNothings :: [[(Bool, Maybe Couleur)]] -> [[(Bool, Couleur)]]
+virerNothings grilleAvecMaybe = map transform' grilleAvecMaybe
+
+-- | Lift value in the 'Maybe' and abandon 'Nothing'.
+transform' :: [(a, Maybe b)] -> [(a, b)]
+transform' = map (\(a, Just b) -> (a, b)) . filter (isJust . snd)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,14 +6,107 @@ import Lib
 main :: IO ()
 main = hspec $ do
     describe "Hauteur de la grille" $ do
-        it "Pour une grille vide, la première ligne vide de la colonne 1 est 1" $ do
-            (premiereLigneVide (M.fromList []) 1) `shouldBe` 1
-        it "Pour une colonne avec un élément, la première ligne vide est 2" $ do
-            (premiereLigneVide (M.fromList [((1, 1), Bleu)]) 1) `shouldBe` 2
-        it "Pour une colonne 1 avec un élément, la première ligne vide de la colonne 2 est 1" $ do
-            (premiereLigneVide (M.fromList [((1, 1), Bleu)]) 2) `shouldBe` 1
+        it "Pour une grille vide, la hauteur de la colonne 0 est 0" $ do
+            (hauteur [[], [], [], [], [], []] 0) `shouldBe` 0
+        it "Pour une colonne avec un élément, la hauteur est 1" $ do
+            (hauteur [[Bleu], [], [], [], [], []] 0) `shouldBe` 1
+        it "Pour une colonne 1 avec un élément, la hauteur de la colonne 1 est 0" $ do
+            (hauteur [[Bleu], [], [], [], [], []] 1) `shouldBe` 0
     describe "Ajout de bloc" $ do
-        it "Si on ajoute un bloc, la première ligne vide doit être 2" $ do
-            (premiereLigneVide (ajouter (M.fromList []) 1 Bleu) 1) `shouldBe` 2
-        it "Si on ajoute deux blocs, la première ligne vide doit être 3" $ do
-            (premiereLigneVide (ajouterDeuxBlocs (M.fromList []) 1 Bleu) 1) `shouldBe` 3
+        it "Si on ajoute un bloc, la hauteur doit être 1" $ do
+            (hauteur (ajouter 0 Bleu [[], [], [], [], [], []]) 0) `shouldBe` 1
+        it "Si on ajoute deux blocs, la hauteur doit être 2" $ do
+            (hauteur (ajouterDeuxBlocs 0 Bleu [[], [], [], [], [], []]) 0) `shouldBe` 2
+        it "Si on ajoute un bloc, il doit être présent" $ do
+            (ajouter 5 Bleu [[], [], [], [], [], []]) `shouldBe` [[], [], [], [], [], [Bleu]]
+        it "Si on ajoute deux blocs, ils doivent être présents" $ do
+            (ajouterDeuxBlocs 5 Bleu [[], [], [], [], [], []]) `shouldBe` [[], [], [], [], [], [Bleu, Bleu]]
+        it "Si on ajoute un bloc sur un existant, il doit être au dessus" $ do
+            (ajouter 5 Vert [[], [], [], [], [], [Bleu]]) `shouldBe` [[], [], [], [], [], [Bleu, Vert]]
+    describe "Bloc marque de la même couleur" $ do
+        it "Ne change pas la grille si l'on demande une colonne inférieure à 0" $ do
+            (blocEstNonMarqueEtMemeCouleur Bleu (-1) 2 grilleComplexeMaybe) `shouldBe` grilleComplexeMaybe
+        it "Ne change pas la grille si l'on demande une ligne inférieure à 0" $ do
+            (blocEstNonMarqueEtMemeCouleur Bleu 2 (-1) grilleComplexeMaybe) `shouldBe` grilleComplexeMaybe
+        it "Ne change pas la grille si l'on demande une colonne supérieure à 6" $ do
+            (blocEstNonMarqueEtMemeCouleur Bleu 6 2 grilleComplexeMaybe) `shouldBe` grilleComplexeMaybe
+        it "Ne change pas la grille si l'on demande une ligne supérieure à sa hauteur" $ do
+            (blocEstNonMarqueEtMemeCouleur Bleu 0 3 grilleComplexeMaybe) `shouldBe` grilleComplexeMaybe
+        it "Ne change pas la grille si l'on demande une ligne vide" $ do
+            (blocEstNonMarqueEtMemeCouleur Bleu 5 0 grilleComplexeMaybe) `shouldBe` grilleComplexeMaybe
+        it "Ne change rien si la marque est déjà posée et que la couleur correspond" $ do
+            (blocEstNonMarqueEtMemeCouleur Vert 0 0 (modifierElement (const (True, Just Vert)) 0 0 grilleComplexeMaybe)) `shouldBe` (modifierElement (const (True, Just Vert)) 0 0 grilleComplexeMaybe)
+        it "Ne change rien si la marque est déjà posée et que la couleur ne correspond pas" $ do
+            (blocEstNonMarqueEtMemeCouleur Vert 0 0 (modifierElement (const (True, Just Bleu)) 0 0 grilleComplexeMaybe)) `shouldBe` (modifierElement (const (True, Just Bleu)) 0 0 grilleComplexeMaybe)
+        it "Ne change rien si la couleur n'est pas bonne" $ do
+            (blocEstNonMarqueEtMemeCouleur Vert 0 0 grilleComplexeMaybe) `shouldBe` grilleComplexeMaybe
+        it "Change la marque et le maybe si la couleur est bonne" $ do
+            (blocEstNonMarqueEtMemeCouleur Vert 0 2 grilleComplexeMaybe) `shouldBe` (modifierElement (const (True, Nothing)) 0 2 grilleComplexeMaybe)
+    describe "Parcourir la grille" $ do
+        it "Marque le vert seulement" $ do
+            (parcourir Vert 0 2 (supprimerBloc 0 2 grilleComplexeMaybe) 10) `shouldBe` (supprimerBloc 0 2 grilleComplexeMaybe, 10)
+        it "Marque le vert seulement si le bloc d'en dessous est supprimé" $ do
+            (parcourir Vert 0 2 (supprimerBloc 0 1 $ supprimerBloc 0 2 grilleComplexeMaybe) 10) `shouldBe` (supprimerBloc 0 1 $ supprimerBloc 0 2 grilleComplexeMaybe, 10)
+        it "Marque et supprime les trois rouges depuis la gauche" $ do
+            (parcourir Rouge 0 1 (supprimerBloc 0 1 grilleComplexeMaybe) 1) `shouldBe` (supprimerBloc 2 1 $ supprimerBloc 0 1 $ supprimerBloc 1 1 grilleComplexeMaybe, 3)
+        it "Marque et supprime les trois rouges depuis le centre" $ do
+            (parcourir Rouge 1 1 (supprimerBloc 1 1 grilleComplexeMaybe) 1) `shouldBe` (supprimerBloc 2 1 $ supprimerBloc 0 1 $ supprimerBloc 1 1 grilleComplexeMaybe, 3)
+        it "Marque et supprime les sept bleus" $ do
+            (parcourir Bleu 0 0 (supprimerBloc 0 0 grilleComplexeMaybe) 1) `shouldBe` (grilleBleuMaybe, 7)
+    describe "Parcourir à partir de…" $ do
+        it "Marque seulement le vert sans le supprimer" $ do
+            (parcourirGrilleAPartirDe 0 2 grilleComplexeSansMaybe) `shouldBe` (modifierElement (const (True , Vert)) 0 2 grilleComplexeSansMaybe, 0)
+        it "Marque seulement les rouges sans le supprimer" $ do
+            (parcourirGrilleAPartirDe 0 1 grilleComplexeSansMaybe) `shouldBe` (modifierElement (const (True , Rouge)) 2 1 $ modifierElement (const (True , Rouge)) 1 1 $ modifierElement (const (True , Rouge)) 0 1 grilleComplexeSansMaybe, 0)
+        it "Marque les bleus et les supprime" $ do
+            (parcourirGrilleAPartirDe 0 0 grilleComplexeSansMaybe) `shouldBe` (grilleMarqueeSansBleu, 7)
+    describe "simplifier grille marquée" $ do
+        it "Supprime tous les bleus et donne le score" $ do
+            (simplifierGrilleMarquee grilleComplexeSansMaybe 0) `shouldBe` (grilleSansBleuSansRouge, 11)
+        it "Ne supprime pas les roses donc score 0" $ do
+            (simplifierGrilleMarquee (map (map (\x -> (False, x))) $ mauvaiseGrille1) 0) `shouldBe` (mauvaiseGrille1, 0)
+    describe "Simplifier grille" $ do
+        it "Simplifie une grille avec des cranes" $ do
+            (simplifierGrille grilleCrane) `shouldBe` (grilleCraneSimplifiee, 4)
+    describe "Calculer le score" $ do
+        it "Score grille1 bonne colonne" $ do
+            (snd $ simplifierGrille (ajouterDeuxBlocs 4 Rose grille1)) `shouldBe` 4
+        it "Score grille1 mauvaise colonne" $ do
+            (snd $ simplifierGrille (ajouterDeuxBlocs 5 Rose grille1)) `shouldBe` 0
+    describe "Choisir colonne" $ do
+        it "Grille example 1" $ do
+            (choisirColonne Rose grille1) `shouldBe` 4
+
+
+grilleComplexe :: [[Couleur]]
+grilleComplexe = [[Bleu, Rouge, Vert], [Bleu, Rouge], [Bleu, Rouge, Bleu, Rouge], [Bleu, Bleu, Bleu], [], []]
+
+grilleComplexeMaybe :: [[(Bool, Maybe Couleur)]]
+grilleComplexeMaybe = map (map (\couleur -> (False, Just couleur))) grilleComplexe
+
+grilleComplexeSansMaybe :: [[(Bool, Couleur)]]
+grilleComplexeSansMaybe = map (map (\couleur -> (False, couleur))) grilleComplexe
+
+grilleBleuMaybe :: [[(Bool, Maybe Couleur)]]
+grilleBleuMaybe = supprimerBloc 0 0 $ supprimerBloc 1 0 $ supprimerBloc 2 0 $ supprimerBloc 3 0 $ supprimerBloc 3 1 $ supprimerBloc 3 2 $ supprimerBloc 2 2 grilleComplexeMaybe
+
+grilleSansBleu :: [[Couleur]]
+grilleSansBleu = [[Rouge, Vert], [Rouge], [Rouge, Rouge], [], [], []]
+
+grilleSansBleuSansRouge :: [[Couleur]]
+grilleSansBleuSansRouge = [[Vert], [], [], [], [], []]
+
+grilleMarqueeSansBleu :: [[(Bool, Couleur)]]
+grilleMarqueeSansBleu = map (map (\couleur -> (False, couleur))) grilleSansBleu
+
+grille1 :: [[Couleur]]
+grille1 = [[], [], [Jaune, Jaune], [Rouge, Rouge], [Rose, Rose], [Bleu, Bleu]]
+
+mauvaiseGrille1 :: [[Couleur]]
+mauvaiseGrille1 = [[], [], [Jaune, Jaune], [Rouge, Rouge], [Rose, Rose], [Bleu, Bleu, Rose, Rose]]
+
+grilleCrane :: [[Couleur]]
+grilleCrane = [[Crane], [], [Crane, Crane], [Rouge], [Rouge, Rouge, Rouge], []]
+
+grilleCraneSimplifiee :: [[Couleur]]
+grilleCraneSimplifiee = [[Crane], [], [Crane], [], [], []]
